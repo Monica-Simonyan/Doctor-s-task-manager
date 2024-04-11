dbe52f02c26c1764258c6306f3ef15922aa20719
Smonique <121874599+Smonique@users.noreply.github.com>
2024-04-11 23:27:03 +0400

    History Demo

--- History.java
+++ History.java
@@ -1,3 +1,43 @@
-public class History {
+import Utilities.Utility;
 
+import java.util.ArrayList;
+public class History extends Utility {
+    private ArrayList<String> allergies;
+    private ArrayList<String> procedures;
+    private ArrayList<String> prescriptions;
+
+    public History() {
+        allergies = new ArrayList<>();
+        prescriptions = new ArrayList<>();
+        procedures = new ArrayList<>();
+
+    }
+
+    public History(History that) {
+
+    }
+
+    public ArrayList<String> getAllergies() {
+        return allergies;
+    }
+
+    public ArrayList<String> getProcedures() {
+        return procedures;
+    }
+
+    public ArrayList<String> getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void addAllergies(String... allergy) {
+        allergies = appendElementsToArrayList(allergies, allergy);
+    }
+
+    public void addProcedures(String... procedure) {
+        procedures = appendElementsToArrayList(procedures, procedure);
+    }
+
+    public void addPrescriptions(String... prescription) {
+        prescriptions = appendElementsToArrayList(prescriptions, prescription);
+    }
 }
