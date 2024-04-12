df3c61c277f796a7aa10a18f7da7f22e24edea65
Smonique <121874599+Smonique@users.noreply.github.com>
2024-04-12 23:13:33 +0400

    Accessors and mutators

--- patientCategories/MinorPatient.java
+++ patientCategories/MinorPatient.java
@@ -3,4 +3,40 @@ package patientCategories;
 import patient.Patient;
 
 public class MinorPatient extends Patient implements Cloneable {
+    private String motherGuardianName;
+    private String fatherGuardianName;
+    private String motherPhoneNumber;
+    private String fatherGuardianPhoneNumber;
+
+    public String getMotherGuardianName() {
+        return motherGuardianName;
+    }
+
+    public void setMotherGuardianName(String motherGuardianName) {
+        this.motherGuardianName = motherGuardianName;
+    }
+
+    public String getFatherGuardianName() {
+        return fatherGuardianName;
+    }
+
+    public void setFatherGuardianName(String fatherGuardianName) {
+        this.fatherGuardianName = fatherGuardianName;
+    }
+
+    public String getMotherPhoneNumber() {
+        return motherPhoneNumber;
+    }
+
+    public void setMotherPhoneNumber(String motherPhoneNumber) {
+        this.motherPhoneNumber = motherPhoneNumber;
+    }
+
+    public String getFatherGuardianPhoneNumber() {
+        return fatherGuardianPhoneNumber;
+    }
+
+    public void setFatherGuardianPhoneNumber(String fatherGuardianPhoneNumber) {
+        this.fatherGuardianPhoneNumber = fatherGuardianPhoneNumber;
+    }
 }
