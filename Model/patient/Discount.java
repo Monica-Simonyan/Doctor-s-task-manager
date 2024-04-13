0ad47c89e5d9a722c16e81ee5abef849e201732a
Smonique <121874599+Smonique@users.noreply.github.com>
2024-04-13 17:38:54 +0400

    Doc

--- Model/patient/Discount.java
+++ Model/patient/Discount.java
@@ -1,5 +1,10 @@
 package Model.patient;
 
 public interface Discount {
-    public int applyDiscount(int amount);
+    /**Method to apply discount to the given integer amount of money
+     *
+     * @param amount integer amount of money
+     * @return integer money with applied discount
+     */
+     int applyDiscount(int amount);
 }
