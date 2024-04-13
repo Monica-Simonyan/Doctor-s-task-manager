a7d3f5a6c78ccd064da225772dfce4507e8dd8b2
Smonique <121874599+Smonique@users.noreply.github.com>
2024-04-13 18:06:34 +0400

    Doc

--- model/patientCategories/PregnantPatient.java
+++ model/patientCategories/PregnantPatient.java
@@ -1,14 +1,16 @@
 package model.patientCategories;
 
-import model.patient.Payments;
+import model.patient.Discount;
 import model.patient.Patient;
+import model.patient.Payments;
 
 import java.util.ArrayList;
 
-public class PregnantPatient extends Patient implements Cloneable {
+public class PregnantPatient extends Patient implements Cloneable, Discount {
     private int trimester;
     private String contraindications;
     private final double DISCOUNT = 0.2;
+
     /**
      * Adds the given fee to the list of fees
      *
@@ -16,5 +18,18 @@ public class PregnantPatient extends Patient implements Cloneable {
      */
     public void addFee(ArrayList<Payments.Fee> fees, Payments.Fee fee) {
 
+    }
+
+    /**
+     * Applied discount to the given amount of money
+     *
+     * @param amount integer amount of money
+     * @return integer amount of money with discount applied
+     */
+    public int applyDiscount(int amount) {
+        if (amount >= 20)
+            return (int) (amount * 0.9 - 5);
+        else
+            return (int) (amount*0.85);
     }
 }
