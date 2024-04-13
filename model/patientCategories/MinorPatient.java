1a3765f61e65dfbe87267edb04e49e9f5e9776ce
Smonique <121874599+Smonique@users.noreply.github.com>
2024-04-13 18:00:13 +0400

    Doc

--- model/patientCategories/MinorPatient.java
+++ model/patientCategories/MinorPatient.java
@@ -1,12 +1,12 @@
 package model.patientCategories;
 
 import model.patient.Discount;
-import model.patient.Payments;
 import model.patient.Patient;
+import model.patient.Payments;
 
 import java.util.ArrayList;
 
-public class MinorPatient extends Patient implements Discount{
+public class MinorPatient extends Patient implements Discount {
     //instance variables
     private String motherGuardianName;
     private String fatherGuardianName;
@@ -14,30 +14,62 @@ public class MinorPatient extends Patient implements Discount{
     private String fatherGuardianPhoneNumber;
 
     //Accessors and mutators
+
+    /**
+     * Returns the name of mother guardian
+     *
+     * @return name of mother guardian of type String
+     */
     public String getMotherGuardianName() {
         return motherGuardianName;
     }
 
+    /**
+     * Sets the name of mother guardian
+     *
+     * @param motherGuardianName new name of the mother guardian of type String
+     */
     public void setMotherGuardianName(String motherGuardianName) {
         this.motherGuardianName = motherGuardianName;
     }
 
+    /**
+     * Returns the name of father guardian
+     *
+     * @return name of father guardian of type String
+     */
     public String getFatherGuardianName() {
         return fatherGuardianName;
     }
-
+    /**
+     * Sets the name of father guardian
+     *
+     * @param fatherGuardianName new name of the father guardian of type String
+     */
     public void setFatherGuardianName(String fatherGuardianName) {
         this.fatherGuardianName = fatherGuardianName;
     }
-
+    /**
+     * Returns the phone number of mother guardian
+     *
+     * @return phone number of mother guardian of type String
+     */
     public String getMotherPhoneNumber() {
         return motherPhoneNumber;
     }
 
+    /**Sets the phone number of mother guardian
+     *
+     * @param motherPhoneNumber new phone number of mother guardian of type String
+     */
     public void setMotherPhoneNumber(String motherPhoneNumber) {
         this.motherPhoneNumber = motherPhoneNumber;
     }
-
+    /**
+     * Returns the phone number of father guardian
+     *
+     * @return phone number of father guardian of type String
+     */
     public String getFatherGuardianPhoneNumber() {
         return fatherGuardianPhoneNumber;
     }
@@ -46,23 +78,25 @@ public class MinorPatient extends Patient implements Discount{
         this.fatherGuardianPhoneNumber = fatherGuardianPhoneNumber;
     }
 
-    /**Adds fee to the ArrayList of fees in Payments
+    /**
+     * Adds fee to the ArrayList of fees in Payments
      *
      * @param fees ArrayList of fees of type Payments.Fee
-     * @param fee fee of type Payments.Fee
+     * @param fee  fee of type Payments.Fee
      */
     public void addFee(ArrayList<Payments.Fee> fees, Payments.Fee fee) {
         fee.setAmount(applyDiscount(fee.getAmount()));
         fees.add(fee);
     }
 
-    /**Applied discount to the given amount of money
+    /**
+     * Applied discount to the given amount of money
      *
      * @param amount integer amount of money
      * @return integer amount of money with discount applied
      */
     public int applyDiscount(int amount) {
-        amount*=0.2;
+        amount *= 0.2;
         return amount;
     }
 }
