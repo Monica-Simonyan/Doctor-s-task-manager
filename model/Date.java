4a296e625d0f1d12fb39cb7aac38df1496ac374d
Smonique <121874599+Smonique@users.noreply.github.com>
2024-04-13 19:37:15 +0400

    Normal Date

--- model/Date.java
+++ model/Date.java
@@ -1,7 +1,44 @@
 package model;
 
 public class Date {
-    public static void main(String[] args) {
-        System.out.println("l");
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(){
+    }
+    public Date(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public Date(Date that){
+        this.day = that.day;
+        this.month = that.month;
+        this.year = that.year;
+    }
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
