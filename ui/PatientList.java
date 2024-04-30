49ff6da3014ea0a50b604a1ae10c5ff980385740
Monica-Simonyan <monika_simonyan@edu.aua.am>
2024-04-30 21:45:02 +0400

    Constructor

--- ui/PatientList.java
+++ ui/PatientList.java
@@ -12,10 +12,20 @@ public class PatientList extends JFrame {
     ArrayList<Patient> patients;
 
     public PatientList() {
+        JPanel listPanel = new JPanel();
+        listPanel.setLayout(new GridLayout(PatientListItem.count, 1));
+        JScrollPane scrollPane = new JScrollPane(listPanel);
+
+        // Set up scrolling options
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        // Add the scroll pane to the frame
+        add(scrollPane);
         int WIDTH = 300;
         int HEIGHT = 500;
         // Add components to the content pane
-        AddButton addButton = new AddButton(50);
+        AddButton addButton = new AddButton(50, this);
         addButton.setBounds(WIDTH - 70, HEIGHT - 90, 50, 50);
         add(addButton);
 
