809ef4ef53cba2663161c3a42f7580733b3818e8
myLibrary <sona.avagyan05@gmail.com>
2024-05-01 18:41:21 +0400

    search

--- ui/PatientList.java
+++ ui/PatientList.java
@@ -6,6 +6,7 @@ import model.patient.PersonalInformation;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 
@@ -13,10 +14,15 @@ public class PatientList extends JFrame {
     static final int WIDTH = 300;
     static final int HEIGHT = 600;
     ArrayList<Patient> patients;
+    JPanel searchPanel;
+    JPanel listPanel;
+    JTextField search;
+    JTextArea area;
+    JButton searchButton;
 
     public PatientList() {
 
-        JPanel listPanel = new JPanel();
+        listPanel = new JPanel();
         listPanel.setLayout(new GridLayout(0, 1));
         JScrollPane scrollPane = new JScrollPane(listPanel);
 
@@ -43,6 +49,18 @@ public class PatientList extends JFrame {
         listPanel.add(new PatientListItem( "mo", "lastName", 14, PersonalInformation.Gender.FEMALE));
         listPanel.add(new PatientListItem("mno", "lastName", 14, PersonalInformation.Gender.FEMALE));
 
+        search = new JTextField(5);
+        searchButton = new JButton("Search");
+        area = new JTextArea(10, 30);
+        area.setEditable(false);
+
+        searchButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //search();
+            }
+        });
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setSize(new Dimension(WIDTH, HEIGHT));
@@ -50,8 +68,35 @@ public class PatientList extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
         setTitle("Patient List");
+
+        searchPanel = new JPanel();
+        searchPanel.add(new JLabel("Find patient..."));
+        searchPanel.add(search);
+        searchPanel.add(searchButton);
+
+        getContentPane().add(searchPanel, "North");
+        getContentPane().add(new JScrollPane(area), "Center");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
+    /*
+    private void search() {
+        String query = search.getText();
+        StringBuilder results = new StringBuilder();
+        for (String item : listPanel) {
+            if (item.toLowerCase().contains(query.toLowerCase())) {
+                results.append(item).append("\n");
+            }
+        }
+        area.setText(results.toString());
+    }
+
+
+     */
     public static void main(String[] args) {
         new PatientList();
     }
---
+++ ui/SearchBar.java
@@ -0,0 +1,30 @@
+package ui;
+
+import model.patient.Patient;
+import model.patient.Payments;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class SearchBar extends JFrame {
+    ArrayList<Patient> patients;
+
+    public SearchBar(){
+        JTextField searchField = new JTextField(30);
+
+    }
+
+    /*
+    searchField.setLayout(new BorderLayout());
+    JLabel label = new JLabel(icon);
+    label.setCursor(Cursor.getDefaultCursor());
+    searchField.add(label, BorderLayout.LINE_END);
+    label.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            searchField.setText("");
+        }
+    });
+
+     */
+}
