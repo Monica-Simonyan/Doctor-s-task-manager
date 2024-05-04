587fbb1cf501807691062d8a77a717ab5c36a5a7
Monica-Simonyan <monika_simonyan@edu.aua.am>
2024-05-04 14:15:01 +0400

    ....

--- ui/PopupWindows/PatientCardPopup.java
+++ ui/PopupWindows/PatientCardPopup.java
@@ -1,18 +1,18 @@
 package ui.PopupWindows;
 
-import model.patient.Patient;
-import model.patient.PersonalInformation;
+import model.patient.*;
+import ui.InformationPanel;
+
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class PatientCardPopup extends JDialog {
     private static final Color color = new Color(252, 217, 217);
     private static final Color backgroundColor = new Color(222, 227, 235);
-
-    static JPanel t;
+    static JPanel dataContainer;
     static JButton saveAllergy, savePrescription;
     static ImageIcon profile, imgIcon;
     static Image image;
@@ -20,16 +20,21 @@ public class PatientCardPopup extends JDialog {
     static JTextField allergies, prescriptions;
     Patient patient;
     PersonalInformation info;
+    public PatientCardPopup(Patient patient){
+        this.patient = patient.clone();
+        this.info = this.patient.getPersonalInfo();
 
-    public PatientCardPopup(PersonalInformation info) {
-        this.info = info;
-
-        t = new JPanel();
-
-        profile = new ImageIcon("src/ui/149071.png");
-        image = profile.getImage().getScaledInstance(80, 120, Image.SCALE_SMOOTH);
+        //Contianer for profile image and personal information
+        dataContainer = new JPanel();
+        profile = new ImageIcon(patient.getImageURL());
+        image = profile.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        image = profile.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
         imgIcon = new ImageIcon(image);
         img = new JLabel(imgIcon);
+
+        InformationPanel informationPanel = new InformationPanel(patient, new Color(0,0,0,0));
+        informationPanel.setBorder(new EmptyBorder(10,80,0,0));
+        informationPanel.setBorder(new EmptyBorder(10,20,0,0));
 
         name = new JLabel("Name: " + info.getName() + " " + info.getLastName());
         name.setFont(new Font("Serif", Font.PLAIN, 15));
@@ -44,6 +49,7 @@ public class PatientCardPopup extends JDialog {
         lastVisit.setFont(new Font("Serif", Font.PLAIN, 15));
         //
         nextVisit = new JLabel();
+        nextVisit = new JLabel("Next Visit Date: "+ patient.getNextVisitDate().toString().substring(0,19));
         nextVisit.setFont(new Font("Serif", Font.PLAIN, 15));
 
         phone = new JLabel("Phone Number: " + info.getPhoneNumber());
@@ -60,25 +66,19 @@ public class PatientCardPopup extends JDialog {
 
         allergyLabel = new JLabel("Allergies");
         allergyLabel.setFont(new Font("Serif", Font.PLAIN, 18));
-
         allergies = new JTextField("List Allergies", 10);
         allergies.setBackground(backgroundColor);
-
         saveAllergy = new JButton("Save");
-        saveAllergy.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String s = e.getActionCommand();
-                if (s.equals("Save")) {
-                    JOptionPane.showMessageDialog(null, "Saved");
-                    // make allergies seen
-                }
+        saveAllergy.addActionListener(e -> {
+            String s = e.getActionCommand();
+            if (s.equals("Save")) {
+                JOptionPane.showMessageDialog(null, "Saved");
+                // make allergies seen
             }
         });
 
         prescLabel = new JLabel("Allergies");
         prescLabel.setFont(new Font("Serif", Font.PLAIN, 18));
-
         prescriptions = new JTextField("List Prescriptions", 10);
         prescriptions.setBackground(backgroundColor);
         savePrescription = new JButton("Save");
@@ -92,12 +92,16 @@ public class PatientCardPopup extends JDialog {
                 }
             }
         });
+        dataContainer.add(img);
+        informationPanel.add(nextVisit);
+        informationPanel.add(phone);
+        informationPanel.add(email);
+        dataContainer.add(informationPanel);
+        add(dataContainer);
         setPreferredSize(new Dimension(400, 550));
         pack();
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
     }
-
 }
-
