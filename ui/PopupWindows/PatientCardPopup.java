74092e206afea55d01cb668035068ffa5daf2e1f
Monica-Simonyan <monika_simonyan@edu.aua.am>
2024-05-08 00:06:26 +0400

    Dshdshddddddsh

--- ui/PopupWindows/PatientCardPopup.java
+++ ui/PopupWindows/PatientCardPopup.java
@@ -3,15 +3,11 @@ package ui.PopupWindows;
 import model.patient.*;
 
 import ui.Elements.InformationPanel;
-import ui.HomePage;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.io.*;
 import java.util.Scanner;
@@ -19,25 +15,25 @@ import java.util.Scanner;
 public class PatientCardPopup extends JDialog {
     private static final Color backgroundColor = new Color(222, 227, 235);
     private static String fileName;
-    private JPanel dataContainer;
-    private JButton saveAllergy, savePrescription, back;
-    private ImageIcon profile, imgIcon;
-    private Image image;
-    private JLabel img, nextVisit, phone, email, address, allergyLabel, allergyContent, prescriptionLabel;
+    private final JLabel img;
+    private JLabel allergyContent;
+    private JLabel prescriptionLabel;
     private JTextField allergies, prescriptions;
     private String s;
-    private Patient patient;
-    private PersonalInformation info;
+    private final Patient patient;
+    private final PersonalInformation info;
+    private History history;
+    Payments payments;
     private static JLabel totalFeeLabel = new JLabel();
 
     public PatientCardPopup(Patient patient) {
         this.patient = patient.clone();
         this.info = this.patient.getPersonalInfo();
 
-        dataContainer = new JPanel();
-        profile = new ImageIcon(patient.getImageURL());
-        image = profile.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-        imgIcon = new ImageIcon(image);
+        JPanel dataContainer = new JPanel();
+        ImageIcon profile = new ImageIcon(patient.getImageURL());
+        Image image = profile.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon imgIcon = new ImageIcon(image);
         img = new JLabel(imgIcon);
         img.setHorizontalTextPosition(0);
         img.setVerticalTextPosition(0);
@@ -53,24 +49,24 @@ public class PatientCardPopup extends JDialog {
         informationPanel.setBorder(new EmptyBorder(10, 20, 0, 0));
 
         // Add patient information labels
-        nextVisit = new JLabel("Next Visit Date: " + patient.getNextVisitDate().toString().substring(0, 19));
+        JLabel nextVisit = new JLabel("Next Visit Date: " + patient.getNextVisitDate().toString().substring(0, 19));
         nextVisit.setFont(new Font("Serif", Font.PLAIN, 15));
-        phone = new JLabel("Phone Number: " + info.getPhoneNumber());
+        JLabel phone = new JLabel("Phone Number: " + info.getPhoneNumber());
         phone.setFont(new Font("Serif", Font.PLAIN, 15));
-        email = new JLabel("Email Address: " + info.getGmail());
+        JLabel email = new JLabel("Email Address: " + info.getGmail());
         email.setFont(new Font("Serif", Font.PLAIN, 15));
-        address = new JLabel("Address: " + info.getAddress());
+        JLabel address = new JLabel("Address: " + info.getAddress());
         address.setFont(new Font("Serif", Font.PLAIN, 15));
 
         // Add allergy panel
         JPanel allergyPanel = new JPanel();
-        allergyLabel = new JLabel("Allergies");
+        JLabel allergyLabel = new JLabel("Allergies");
         allergyContent = new JLabel();
     //    allergyLabel.setFont(new Font("Serif", Font.PLAIN, 18));
         allergies = new JTextField("List Allergies", 20);
         allergyContent.setPreferredSize(new Dimension(200, 100));
         allergies.setBackground(backgroundColor);
-        saveAllergy = new JButton("Save");
+        JButton saveAllergy = new JButton("Save");
         saveAllergy.addActionListener(e -> {
             s = allergies.getText();
             if (s.equals("Save")) {
@@ -87,19 +83,16 @@ public class PatientCardPopup extends JDialog {
         prescriptionLabel = new JLabel("Prescriptions");
         prescriptions = new JTextField("List Prescriptions", 20);
         prescriptions.setBackground(backgroundColor);
-        savePrescription = new JButton("Save");
-        savePrescription.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String s = prescriptions.getText();
-                if (s.equals("Save")) {
-                    JOptionPane.showMessageDialog(null, "Saved");
-                    prescriptionLabel.setText(s);
-                }
+        JButton savePrescription = new JButton("Save");
+        savePrescription.addActionListener(e -> {
+            String s = prescriptions.getText();
+            if (s.equals("Save")) {
+                JOptionPane.showMessageDialog(null, "Saved");
+                prescriptionLabel.setText(s);
             }
         });
 
-        back = new JButton("Back");
+        JButton back = new JButton("Back");
         back.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -126,7 +119,7 @@ public class PatientCardPopup extends JDialog {
 
         JScrollPane scrollPane = new JScrollPane(panel);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setPreferredSize(new Dimension(400, 400)); // Set preferred size for JScrollPane
+        scrollPane.setPreferredSize(new Dimension(500, 400)); // Set preferred size for JScrollPane
 
         add(scrollPane);
         pack();
@@ -134,8 +127,6 @@ public class PatientCardPopup extends JDialog {
         setLocationRelativeTo(null);
         setVisible(true);
     }
-
-
 
     private JPanel getProcedurePanel() {
         String[] procedures = {"Surgery", "Appendectomy", "Breast biopsy", "Cataract surgery"};
@@ -167,77 +158,70 @@ public class PatientCardPopup extends JDialog {
         Image img = icon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
         icon = new ImageIcon(img);
         JButton addButton = new JButton(icon);
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = new JDialog();
-                dialog.setTitle("Select Procedure");
-                dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-                dialog.setLayout(new GridLayout(0, 1));
-                String[] procedures = {"Surgery", "Appendectomy", "Breast biopsy", "Cataract surgery"};
+        addButton.addActionListener(e -> {
+            JDialog dialog = new JDialog();
+            dialog.setTitle("Select Procedure");
+            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            dialog.setLayout(new GridLayout(0, 1));
+            String[] procedures = { "Appendectomy", "Breast biopsy", "Cataract surgery", "MRI", "CT", "Blood analysis"};
 
-                ButtonGroup buttonGroup = new ButtonGroup();
+            ButtonGroup buttonGroup = new ButtonGroup();
 
-                for (int i = 0; i < procedures.length; i++) {
-                    JRadioButton radioButton = new JRadioButton(procedures[i]);
-                    dialog.add(radioButton);
-                    buttonGroup.add(radioButton);
+            for (int i = 0; i < procedures.length; i++) {
+                JRadioButton radioButton = new JRadioButton(procedures[i]);
+                dialog.add(radioButton);
+                buttonGroup.add(radioButton);
 
-                    final int index = i;
+                final int index = i;
 
-                    radioButton.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            try {
-                                JDialog feeDialog = new JDialog();
-                                feeDialog.setTitle("Select Fee");
-                                feeDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-                                feeDialog.setLayout(new GridLayout(0, 1));
+                radioButton.addActionListener(e1 -> {
+                    try {
+                        JDialog feeDialog = new JDialog();
+                        feeDialog.setTitle("Select Fee");
+                        feeDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+                        feeDialog.setLayout(new GridLayout(0, 1));
 
-                                SpinnerModel spinnerModel = new SpinnerNumberModel(20000, 10000, 2000000, 1000); // Initial value, minimum value, maximum value, step size
-                                JSpinner spinner = getjSpinner(spinnerModel);
+                        SpinnerModel spinnerModel = new SpinnerNumberModel(10000, 5000, 2000000, 1000); // Initial value, minimum value, maximum value, step size
+                        JSpinner spinner = getjSpinner(spinnerModel);
 
-                                JButton confirmButton = new JButton("Confirm Fee");
-                                confirmButton.addActionListener(new ActionListener() {
-                                    @Override
-                                    public void actionPerformed(ActionEvent e) {
-                                        int fee = (int) spinner.getValue();
-                                        String newProcedureText = "<html>Procedure: " + procedures[index] + "<br>Fee: AMD" + fee + "</html>";
-                                        JCheckBox newCheckBox = new JCheckBox(newProcedureText);
-                                        newCheckBox.addActionListener(new ActionListener() {
-                                            @Override
-                                            public void actionPerformed(ActionEvent e) {
-                                                if (!newCheckBox.isSelected()) {
-                                                    totalFeeLabel.setText("Total Fee: AMD" + (extractAmount(totalFeeLabel.getText()) + fee));
-                                                } else {
-                                                    totalFeeLabel.setText("Total Fee: AMD" + (extractAmount(totalFeeLabel.getText()) - fee));
-                                                }
-                                            }
-                                        });
-                                        procedurePanel.add(newCheckBox, procedurePanel.getComponentCount() - 2);
-                                        procedurePanel.revalidate();
-                                        procedurePanel.repaint();
-                                        feeDialog.dispose();
-                                    }
-                                });
+                        JButton confirmButton = new JButton("Confirm Fee");
+                        confirmButton.addActionListener(e11 -> {
+                            payments = new Payments();
+                            history = new History();
+                            int fee = (int) spinner.getValue();
+                            patient.setPayments(payments);
+                            Payments.Fee newFee = new Payments.Fee(false, fee);
+                            patient.addDiscountedFee(newFee);
+                            String newProcedureText = "<html>Procedure: " + procedures[index] + "<br>Fee: AMD" + fee + "</html>";
+                            JCheckBox newCheckBox = new JCheckBox(newProcedureText);
+                            newCheckBox.addActionListener(e111 -> {
+                                if (!newCheckBox.isSelected()) {
+                                    totalFeeLabel.setText("Total Fee: AMD" + (extractAmount(totalFeeLabel.getText()) + fee));
+                                } else {
+                                    totalFeeLabel.setText("Total Fee: AMD" + (extractAmount(totalFeeLabel.getText()) - fee));
+                                }
+                            });
+                            procedurePanel.add(newCheckBox, procedurePanel.getComponentCount() - 2);
+                            procedurePanel.revalidate();
+                            procedurePanel.repaint();
+                            feeDialog.dispose();
+                        });
 
-                                feeDialog.add(spinner);
-                                feeDialog.add(confirmButton);
-                                feeDialog.pack();
-                                feeDialog.setLocationRelativeTo(null);
-                                feeDialog.setVisible(true);
+                        feeDialog.add(spinner);
+                        feeDialog.add(confirmButton);
+                        feeDialog.pack();
+                        feeDialog.setLocationRelativeTo(null);
+                        feeDialog.setVisible(true);
 
-                                dialog.dispose();
-                            } catch (NumberFormatException ex) {
-                                JOptionPane.showMessageDialog(dialog, "Please enter a valid fee amount.");
-                            }
-                        }
-                    });
-                }
-                dialog.pack();
-                dialog.setLocationRelativeTo(null);
-                dialog.setVisible(true);
+                        dialog.dispose();
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(dialog, "Please enter a valid fee amount.");
+                    }
+                });
             }
+            dialog.pack();
+            dialog.setLocationRelativeTo(null);
+            dialog.setVisible(true);
         });
         return addButton;
     }
@@ -245,27 +229,14 @@ public class PatientCardPopup extends JDialog {
     private JSpinner getjSpinner(SpinnerModel spinnerModel) {
         JSpinner spinner = new JSpinner(spinnerModel);
         spinner.setPreferredSize(new Dimension(100, 30));
-        spinner.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-                int amount = (int) spinner.getValue();
-                // Add the amount to the total fee
-                totalFeeLabel.setText("Total Fee: AMD" + amount);
-            }
+        spinner.addChangeListener(e -> {
+            int amount = (int) spinner.getValue();
+            // Add the amount to the total fee
+            totalFeeLabel.setText("Total Fee: AMD" + amount);
         });
         return spinner;
     }
 
-
-
-    private void inputAllergies() {
-        fileName = info.getName() + info.getLastName() + ".pdf";
-        try {
-            PrintWriter outputStream = new PrintWriter(new FileOutputStream(fileName));
-            outputStream.println(s);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
     private int extractAmount(String labelText) {
         int amount = 0;
         String[] parts = labelText.split("AMD");
@@ -278,6 +249,17 @@ public class PatientCardPopup extends JDialog {
         }
         return amount;
     }
+
+    private void inputAllergies() {
+        fileName = info.getName() + info.getLastName() + ".pdf";
+        try {
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream(fileName));
+            outputStream.println(s);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private void showAllergies() {
         fileName = info.getName() + info.getLastName() + ".pdf";
